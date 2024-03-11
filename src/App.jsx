963d5b73db0035b2ac86d8b459328f7e9a432a16
“Jahrulo” <mserayjay@gmail.com>
2024-03-11 22:08:10 +0000

    changes

--- src/App.jsx
+++ src/App.jsx
@@ -192,7 +192,7 @@ const { onAddPost} = usePosts();
 }
 
 function Footer() {
-  return <footer>&copy; The Blog App by Ray Jahrulo ✌️</footer>;
+  return <footer>&copy; 2024 The Blog App by Ray Jahrulo ✌️</footer>;
 }
 
 export default App;
